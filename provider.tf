a855c826820615e0aa54dff3de40b5ea46414561
Aarti chaple <aartichaple2124@gmail.com>
2025-12-25 11:36:05 +0530

    test

--- provider.tf
+++ provider.tf
@@ -3,3 +3,12 @@ provider "azurerm" {
   subscription_id = "0aa6e6f6-6e44-47f7-b30d-2aa0dfd4e5f4"
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "RG"
+    storage_account_name  = "rttfstatestorage"
+    container_name        = "tfstate"
+    key                   = "dev.tfstate"
+  }
+}
+
